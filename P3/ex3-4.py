480d669e75205b5f48c50093a107c8e759571c0d
Webb <1323668359@qq.com>
2018-11-24 00:48:47 +0800

    chore:练习1-3

---
+++ P3/ex3-4.py
@@ -0,0 +1,29 @@
+print("I will now count my chickens:")
+
+print("Hens", 25.0 + 30.0 / 6.0)  #30.0：（25 加（30 除以 6 等于 5））  等于 30
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0) #97：（100 减 （（25 乘 3 等于 75） 取余 4 等于 3）） 等于 97
+
+print("Now I will count the eggs:")
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)    #6.75： 1、3+2+1-5+6=7；2、4%2=0；3、-1/4=-0.25; 4、7-0.25=6.75
+
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+print(3.0 + 2.0 < 5.0 - 7.0)    #False
+
+print("What is 3.0 + 2.0?", 3.0 + 2.0)  #5
+print("What is 5.0 - 7.0?", 5.0 - 7.0)  #-2
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2.0) #True
+print("Is it greater or equal?", 5.0 >= -2.0)   #True
+print("Is it less or equal?", 5.0 <= -2.0)  #False
+
+# +   加号  加
+# -   减号  减
+# /   斜杠  除（结果带小数位，为浮点型）
+# *   星号  乘
+# %   百分号 取余
---
+++ P3/ex3.py
@@ -0,0 +1,29 @@
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)  #30.0：（25 加（30 除以 6 等于 5））  等于 30
+print("Roosters", 100 - 25 * 3 % 4) #97：（100 减 （（25 乘 3 等于 75） 取余 4 等于 3）） 等于 97
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)    #6.75： 1、3+2+1-5+6=7；2、4%2=0；3、-1/4=-0.25; 4、7-0.25=6.75
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)    #False
+
+print("What is 3 + 2?", 3 + 2)  #5
+print("What is 5 - 7?", 5 - 7)  #-2
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2) #True
+print("Is it greater or equal?", 5 >= -2)   #True
+print("Is it less or equal?", 5 <= -2)  #False
+
+# +   加号  加
+# -   减号  减
+# /   斜杠  除（结果带小数位，为浮点型）
+# *   星号  乘
+# %   百分号 取余
